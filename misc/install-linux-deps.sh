de52d2983f23be91062dfe99fd8d1f9414974873
bqqbarbhg <samuli.1995@hotmail.com>
2020-05-06 22:28:24 +0300

    Split Linux setup util

---
+++ misc/install-linux-deps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt install xorg-dev libcurl4-openssl-dev libgl1-mesa-dev
+
--- misc/install-linux-premake5.sh
+++ misc/install-linux-premake5.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
-if which premake5
-then
-	echo "Found global premake5"
-elif [ -f premake5 ]
+if [ -f premake5 ]
 then
 	echo "Found local ./premake5"
 else
-	echo "Downloading premake5 to local ./premake5"
+	echo "Downloading premake5..."
 	curl -L https://github.com/premake/premake-core/releases/download/v5.0.0-alpha15/premake-5.0.0-alpha15-linux.tar.gz -o premake5.tar.gz
 	tar -xf premake5.tar.gz
 	rm premake5.tar.gz
 fi
-
-sudo apt install xorg-dev libcurl4-openssl-dev libgl1-mesa-dev
 
