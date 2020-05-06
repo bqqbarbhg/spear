59cd0b87b25e4bee8408e2207ba2eff5d1c73a09
bqqbarbhg <samuli.1995@hotmail.com>
2020-05-06 20:44:47 +0300

    Fix Clang warning

--- src/sp/ContentFile.cpp
+++ src/sp/ContentFile.cpp
@@ -208,7 +208,7 @@ static void fetchCacheCallback(const sfetch_response_t *response)
 
 		{
 			sf::MutexGuard mg(data->package->cacheMutex);
-			for  (const char *end = data->destinationFile; end = strpbrk(end, "\\/"); end++) { 
+			for  (const char *end = data->destinationFile; (end = strpbrk(end, "\\/")) != nullptr; end++) { 
 				sf::createDirectory(sf::String(data->destinationFile, end - data->destinationFile));
 			}
 			sf::writeFile(sf::String(data->destinationFile), response->buffer_ptr, response->fetched_size);
