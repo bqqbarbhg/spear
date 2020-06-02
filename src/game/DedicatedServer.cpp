1d51cc6ba372164db45c2ddb880f47d6e3e74371
bqqbarbhg <samuli.1995@hotmail.com>
2020-06-02 20:00:19 +0300

    Add dedicated server logging

--- src/game/DedicatedServer.cpp
+++ src/game/DedicatedServer.cpp
@@ -1,8 +1,10 @@
 #if defined(SP_DEDICATED_SERVER)
 
+#include "sf/Base.h"
 #include "ServerMain.h"
 #include "ext/sokol/sokol_time.h"
 #include "bq_websocket_platform.h"
+
 
 // TODO: Replace this
 #include <thread>
@@ -18,6 +20,18 @@ int main(int argc, char **argv)
 
     int port = 4004;
 	server = serverInit(port);
+	if (!server) {
+		char desc[4096];
+		bqws_pt_error err;
+		if (bqws_pt_get_error(&err)) {
+			bqws_pt_get_error_desc(desc, sizeof(desc), &err);
+		} else {
+			snprintf(desc, sizeof(desc), "Unknown error");
+		}
+		sf::debugPrintLine("Failed to setup server: %s", desc);
+
+		return NULL;
+	}
 
 	for (;;) {
 		serverUpdate(server);
