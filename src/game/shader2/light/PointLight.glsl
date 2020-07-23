d6431a44883e178e2f9c02da423322857519c97e
bqqbarbhg <samuli.1995@hotmail.com>
2020-07-23 20:39:05 +0300

    Skip BRDF when in shadow

--- src/game/shader2/light/PointLight.glsl
+++ src/game/shader2/light/PointLight.glsl
@@ -17,27 +17,30 @@ vec3 evaluatePointLight(vec3 P, vec3 N, vec3 V, vec3 cdiff, vec3 f0, float alpha
 	float distSq = dot(delta, delta);
 	vec3 L = delta / sqrt(distSq);
 
+	vec3 result = asVec3(0.0);
+
 	float radiusSq = lightRadius*lightRadius;
 	if (distSq < radiusSq) {
-
 		vec3 lightColor = data1.xyz;
 		vec3 shadowMul = data2.xyz;
 		vec3 shadowBias = data3.xyz;
 		vec3 shadowTexCoord = delta * shadowMul + shadowBias;
 		float shadow = evaluateShadowGrid(shadowTexCoord);
-		float attenuation = 1.0 / (0.1 + distSq) - 1.0 / (0.1 + radiusSq);
-		float linearAttenuation = 1.0 - sqrt(distSq) / lightRadius;
 
-		vec3 H = normalize(L + V);
-		float VdotH = saturate(dot(V, H));
-		float NdotL = saturate(dot(N, L));
-		float NdotV = saturate(dot(N, V));
-		float NdotH = saturate(dot(N, H));
+		if (shadow > 0.0) {
+			float attenuation = 1.0 / (0.1 + distSq) - 1.0 / (0.1 + radiusSq);
+			float linearAttenuation = 1.0 - sqrt(distSq) / lightRadius;
 
-		vec3 result = asVec3(shadow * attenuation * NdotL) * lightColor * BRDF_specularGGX(cdiff, f0, alpha2, VdotH, NdotL, NdotV, NdotH);
-		result += cdiff * saturate(abs(N.y)) * shadow * linearAttenuation * lightColor * 0.005;
-		return result;
-	} else {
-		return asVec3(0.0);
+			vec3 H = normalize(L + V);
+			float VdotH = saturate(dot(V, H));
+			float NdotL = saturate(dot(N, L));
+			float NdotV = saturate(dot(N, V));
+			float NdotH = saturate(dot(N, H));
+
+			result = asVec3(shadow * attenuation * NdotL) * lightColor * BRDF_specularGGX(cdiff, f0, alpha2, VdotH, NdotL, NdotV, NdotH);
+			result += cdiff * saturate(abs(N.y)) * shadow * linearAttenuation * lightColor * 0.005;
+		}
 	}
+
+	return result;
 }
