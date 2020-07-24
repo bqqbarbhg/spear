d59a2896afec32e22aadd1f544fada02f4a96e87
Samuli <bqqbarbhg@users.noreply.github.com>
2020-07-24 12:45:21 +0300

    Typo fix

--- src/ext/sokol/sokol_app_impl.h
+++ src/ext/sokol/sokol_app_impl.h
@@ -2460,7 +2460,7 @@ _SOKOL_PRIVATE void _sapp_run(const sapp_desc* desc) {
     attrs.preserveDrawingBuffer = _sapp.desc.html5_preserve_drawing_buffer;
     attrs.enableExtensionsByDefault = true;
 
-    // bqq patch: Prefer HIHG_PERFORMANCE
+    // bqq patch: Prefer HIGH_PERFORMANCE
     attrs.powerPreference = EM_WEBGL_POWER_PREFERENCE_HIGH_PERFORMANCE;
 
     #if defined(SOKOL_GLES3)
