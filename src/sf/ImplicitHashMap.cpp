ae7ffba812520325fcf808dae98c92612af9900b
bqqbarbhg <samuli.1995@hotmail.com>
2020-08-01 22:59:01 +0300

    Add sf::ImplicitHashMap

---
+++ src/sf/ImplicitHashMap.cpp
@@ -0,0 +1,90 @@
+#include "ImplicitHashMap.h"
+#include "Reflection.h"
+
+namespace sf {
+
+struct ImplicitHashMapType final : Type
+{
+	uint32_t (*hashFn)(void *inst);
+
+	ImplicitHashMapType(const TypeInfo &info, Type *kvType, uint32_t (*hashFn)(void *inst))
+		: Type("sf::ImplicitHashMap", info, HasArray | HasArrayResize)
+		, hashFn(hashFn)
+	{
+		elementType = kvType;
+	}
+
+	virtual void getName(sf::StringBuf &buf)
+	{
+		buf.append("sf::ImplicitHashMap<");
+		elementType->getName(buf);
+		buf.append(">");
+	}
+
+	virtual VoidSlice instGetArray(void *inst)
+	{
+		ImplicitHashMapBase *map = (ImplicitHashMapBase*)inst;
+		return { map->data, map->map.size };
+	}
+
+	virtual VoidSlice instArrayReserve(void *inst, size_t size)
+	{
+		uint32_t kvSize = elementType->info.size;
+		Type *elemType = elementType;
+
+		ImplicitHashMapBase *map = (ImplicitHashMapBase*)inst;
+		if (map->map.capacity < size) {
+			size_t count, allocSize;
+			rhmap_grow(&map->map, &count, &allocSize, size, 0.8);
+
+			void *newAlloc = memAlloc(allocSize + count * kvSize);
+			void *newData = (char*)newAlloc + allocSize;
+			if (map->map.size) {
+				elemType->info.moveRange(newData, map->data, map->map.size);
+			}
+			map->data = newData;
+
+			void *oldAlloc = rhmap_rehash(&map->map, count, allocSize, newAlloc);
+			memFree(oldAlloc);
+		}
+
+		if (size > map->map.size) {
+			char *base = (char*)map->data + map->map.size * kvSize;
+			elemType->info.constructRange(base, size - map->map.size);
+		}
+
+		return { map->data, map->map.capacity };
+	}
+
+	virtual void instArrayResize(void *inst, size_t size)
+	{
+		uint32_t kvSize = elementType->info.size;
+
+		ImplicitHashMapBase *map = (ImplicitHashMapBase*)inst;
+		sf_assert(map->map.capacity >= size);
+		if (map->map.size > size) {
+			char *base = (char*)map->data + map->map.size * kvSize;
+			char *ptr = base;
+			for (uint32_t i = map->map.size; i < size; i++) {
+				uint32_t h = hashFn(ptr), scan = 0;
+				rhmap_find_value(&map->map, h, &scan, i);
+				rhmap_remove(&map->map, h, scan);
+				ptr += kvSize;
+			}
+			elementType->info.destructRange(base, map->map.size - size);
+		}
+		char *ptr = (char*)map->data + map->map.size * kvSize;
+		for (uint32_t i = map->map.size; i < size; i++) {
+			uint32_t h = hashFn(ptr), scan = 0;
+			rhmap_insert(&map->map, h, scan, i);
+			ptr += kvSize;
+		}
+	}
+};
+
+void initImplicitHashMapType(Type *t, const TypeInfo &info, Type *kvType, uint32_t (*hashFn)(void *inst))
+{
+	new (t) ImplicitHashMapType(info, kvType, hashFn);
+}
+
+}
---
+++ src/sf/ImplicitHashMap.h
@@ -0,0 +1,244 @@
+#pragma once
+
+#include "Base.h"
+#include "ext/rhmap.h"
+
+namespace sf {
+
+struct ImplicitHashMapBase
+{
+	rhmap map;
+	void *data;
+};
+
+template <typename T, typename KeyFn>
+struct ImplicitHashMap
+{
+	typedef T Entry;
+	rhmap map;
+	Entry *data;
+
+	ImplicitHashMap()
+	{
+		memset(this, 0, sizeof(ImplicitHashMap));
+	}
+
+	ImplicitHashMap(const ImplicitHashMap &rhs)
+	{
+		memset(this, 0, sizeof(ImplicitHashMap));
+		reserve(rhs.map.size);
+		for (Entry &entry : rhs) {
+			insert(entry);
+		}
+	}
+
+	ImplicitHashMap(ImplicitHashMap &&rhs)
+	{
+		data = rhs.data;
+		map = rhs.map;
+		rhmap_reset(&rhs.map);
+		rhs.data = nullptr;
+	}
+
+	ImplicitHashMap& operator=(const ImplicitHashMap &rhs)
+	{
+		if (&rhs == this) return *this;
+		clear();
+		reserve(rhs.size);
+		for (Entry &entry : rhs) {
+			insert(entry);
+		}
+		return *this;
+	}
+
+	ImplicitHashMap& operator=(ImplicitHashMap &&rhs)
+	{
+		if (&rhs == this) return *this;
+		data = rhs.data;
+		map = rhs.map;
+		rhmap_reset(&rhs.map);
+		rhs.data = nullptr;
+		return *this;
+	}
+
+	~ImplicitHashMap()
+	{
+		destructRangeImp<Entry>(data, map.size);
+		void *oldAlloc = rhmap_reset(&map);
+		memFree(oldAlloc);
+	}
+
+	sf_forceinline uint32_t size() const { return map.size; }
+	sf_forceinline uint32_t capacity() const { return map.capacity; }
+	sf_forceinline Entry *begin() { return data; }
+	sf_forceinline Entry *end() { return data + map.size; }
+	sf_forceinline const Entry *begin() const { return data; }
+	sf_forceinline const Entry *end() const { return data + map.size; }
+
+	void clear()
+	{
+		rhmap_clear(&map);
+	}
+
+	void reserve(uint32_t size)
+	{
+		if (size > map.capacity) {
+			growImp(size);
+		}
+	}
+
+	template <typename KT>
+	Entry &operator[](const KT &key)
+	{
+		uint32_t index;
+		if (insertImp(key, index)) {
+			new (&data[index]) Entry();
+			new (&KeyFn()(data[index])) Key(key);
+		}
+		return data[index];
+	}
+
+	template <typename KT>
+	Entry *find(const KT &key)
+	{
+		uint32_t index;
+		uint32_t h = hash(key), scan = 0;
+		while (rhmap_find(&map, h, &scan, &index)) {
+			if (key == KeyFn()(data[index])) {
+				return &data[index];
+			}
+		}
+		return nullptr;
+	}
+
+	template <typename KT>
+	sf_forceinline const Entry *find(const KT &key) const
+	{
+		return const_cast<ImplicitHashMap*>(this)->find(key);
+	}
+
+	template <typename KT>
+	InsertResult<Entry> insert(const KT &key)
+	{
+		uint32_t index;
+		bool inserted = insertImp(key, index);
+		Entry *entry = &data[index];
+		if (inserted) {
+			new (&data[index]) Entry();
+			new (&data[index].implicitKeyRef()) Key(key);
+		}
+		return { *entry, inserted };
+	}
+
+	InsertResult<Entry> insert(const Entry &entry)
+	{
+		uint32_t index;
+		bool inserted = insertImp(KeyFn()((Entry&)entry), index);
+		Entry *slot = &data[index];
+		if (inserted) {
+			new (&slot) Entry(entry);
+		}
+		return { *slot, inserted };
+	}
+
+	InsertResult<Entry> insert(Entry &&entry)
+	{
+		uint32_t index;
+		bool inserted = insertImp(KeyFn()((Entry&)entry), index);
+		Entry *slot = &data[index];
+		if (inserted) {
+			new (&slot) Entry(std::move(entry));
+		}
+		return { *slot, inserted };
+	}
+
+	InsertResult<Entry> insertOrAssign(const Entry &entry)
+	{
+		uint32_t index;
+		bool inserted = insertImp(KeyFn()((Entry&)entry), index);
+		Entry *slot = &data[index];
+		if (!inserted) {
+			slot->~Entry();
+		}
+		new (&slot) Entry(entry);
+		return { *slot, inserted };
+	}
+
+	template <typename KT>
+	bool remove(const KT &key)
+	{
+		uint32_t index;
+		uint32_t h = hash(key), scan = 0;
+		while (rhmap_find(&map, h, &scan, &index)) {
+			if (key == KeyFn()(data[index])) {
+				rhmap_remove(&map, h, scan);
+				if (index < map.size) {
+					Entry &swap = data[map.size];
+					rhmap_update_value(&map, hash(KeyFn()(swap)), map.size, index);
+					data[index].~Entry();
+					new (&data[index]) Entry(std::move(swap));
+				}
+				data[map.size].~Entry();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	Entry *removeAt(Entry *it)
+	{
+		bool res = remove(it->key);
+		sf_assert(res);
+		return it;
+	}
+
+protected:
+
+	template <typename KT>
+	bool insertImp(const KT &key, uint32_t &index)
+	{
+		if (map.size >= map.capacity) {
+			growImp(128 / sizeof(Entry));
+		}
+
+		uint32_t h = hash(key), scan = 0;
+		while (rhmap_find(&map, h, &scan, &index)) {
+			if (key == KeyFn()(data[index])) {
+				return false;
+			}
+		}
+
+		index = map.size;
+		rhmap_insert(&map, h, scan, index);
+		return true;
+	}
+
+	void growImp(uint32_t size)
+	{
+		size_t count, allocSize;
+		rhmap_grow(&map, &count, &allocSize, size, 0.8);
+
+		void *newAlloc = memAlloc(allocSize + count * sizeof(Entry));
+		Entry *newData = (Entry*)((char*)newAlloc + allocSize);
+		moveRangeImp<Entry>(newData, data, map.size);
+		data = newData;
+
+		void *oldAlloc = rhmap_rehash(&map, count, allocSize, newAlloc);
+		memFree(oldAlloc);
+	}
+};
+
+template <typename T, typename KeyFn> struct IsZeroInitializable<ImplicitHashMap<T, KeyFn>> { enum { value = 1 }; };
+
+void initImplicitHashMapType(Type *t, const TypeInfo &info, Type *entryType, uint32_t (*hashFn)(void *inst));
+
+template <typename T, typename KeyFn>
+struct InitType<ImplicitHashMap<T, KeyFn>> {
+	static void init(Type *t) {
+		return initImplicitHashMapType(t, getTypeInfo<ImplicitHashMap<T>>(), typeOfRecursive<T>(),
+		[](void *inst) { return hash(KeyFn()(*(T*)inst)); });
+	}
+};
+
+
+}
