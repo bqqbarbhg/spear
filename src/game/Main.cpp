f31b89f4200c4d513bb3b38c6697022a8b12389c
bqqbarbhg <samuli.1995@hotmail.com>
2020-08-29 17:59:47 +0300

    Buffer more frames on non-threaded WASM

--- src/game/Main.cpp
+++ src/game/Main.cpp
@@ -44,7 +44,11 @@ void spConfig(sp::MainConfig &config)
 	config.sgDesc.pass_pool_size = 10*1024;
 
 	config.saudioDesc.num_channels = 2;
-	config.saudioDesc.buffer_frames = 1024;
+	#if SF_OS_EMSCRIPTEN && !SF_USE_PTHREADS
+		config.saudioDesc.buffer_frames = 2048;
+	#else
+		config.saudioDesc.buffer_frames = 1024;
+	#endif
 }
 
 sp::FontRef font;
