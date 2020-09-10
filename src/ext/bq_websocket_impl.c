8a8c9aa2363101bcb0a9caffb78ca822750661cf
bqqbarbhg <samuli.1995@hotmail.com>
2020-09-10 18:48:06 +0300

    BQWS fix pt. 2

--- src/ext/bq_websocket_impl.c
+++ src/ext/bq_websocket_impl.c
@@ -3,7 +3,7 @@
 
 #define bqws_malloc(size) sf_malloc(size)
 #define bqws_realloc(ptr, old_size, new_size) sf_realloc(ptr, new_size)
-#define bqws_free(ptr) sf_free(ptr)
+#define bqws_free(ptr, size) sf_free(ptr)
 
 #include "bq_websocket.c.h"
 #include "bq_websocket_platform.c.h"
